f5ab9d20194996e275762b38dc43b9b31bd58ddb
Al Curry <alcurry98@gmail.com>
2018-03-20 20:53:22 -0400

    initial commit

---
+++ assets/js/portfolio.js
@@ -0,0 +1,16 @@
+/* $(document).ready(function() {
+    $('.slideshow').cycle({
+		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+});
+ */
+var image=document.getElementById("image");
+var currentPos = 0;
+var images = ["assets/images/ACReichstag2.jpg", "assets/images/AC95resize2.jpg","assets/images/ACPinesBeach.jpg"]
+
+function volgendefoto() {
+		if (++currentPos >= images.length) currentPos = 0;
+		image.src = images[currentPos];
+}
+
+setInterval(volgendefoto, 3000);
