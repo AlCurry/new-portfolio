f2668833e790e8818cab6213f6318874bf3a2fcf
Al Curry <alcurry98@gmail.com>
2018-03-24 18:40:01 -0400

    update portfolio, font

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,9 +1,3 @@
-/* $(document).ready(function() {
-    $('.slideshow').cycle({
-		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-	});
-});
- */
 var image=document.getElementById("image");
 var currentPos = 0;
 var images = ["assets/images/ACReichstag2.jpg", "assets/images/AC95resize2.jpg","assets/images/ACPinesBeach.jpg"]
